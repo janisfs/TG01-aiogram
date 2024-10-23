59b11c6f48b28e6c5604df475896538e9e2491af
janisfs <janisfs@mail.ru>
2024-10-23 23:21:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import asyncio
-from aiogram import (Bot, Dispatcher, executor
+from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 from config import TOKEN
@@ -15,5 +15,13 @@ async def start(message: Message):
 async def on_startup(_):
     await dp.start_polling(bot)
 
+async def main():
+    await on_startup(dp)
+    await dp.start_polling(bot)
+
 if __name__ == "__main__":
     asyncio.run(main())
+
+
+# my_pal_bot
+# best_pal_bot
